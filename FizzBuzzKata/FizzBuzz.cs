976cb5d56014bfa9c91f27bf0f4fc468fbebcf3a
jvaquero <jvaquero@aidacanarias.com>
2022-02-03 07:18:34 +0000

    Add kata description

--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -2,6 +2,12 @@
 
 namespace FizzBuzzKata
 {
+    /// <summary>
+    /// For numbers from 1 to 100, returns the text representation
+    /// For numbers being multiple of 3, returns "FIZZ"
+    /// For numbers being multiple of 5, returns "BUZZ"
+    /// For numbers being multiple of 3 and 5, returns "FIZZBUZZ"
+    /// </summary>
     public class FizzBuzz
     {
         public static string NumberToText(int number) =>
