c171ecbe5ee33341848332a3efb79f984a76a0b7
jvaquero <jvaquero@aidacanarias.com>
2022-02-03 07:05:48 +0000

    Red returns_two_as_text_for_2

--- FizzBuzzKata.Tests/FizzBuzzShould.cs
+++ FizzBuzzKata.Tests/FizzBuzzShould.cs
@@ -3,10 +3,17 @@
 namespace FizzBuzzKata.Tests {
     public class FizzBuzzShould {
         [Test]
-        public void returns_one_as_tests_for_1()
+        public void returns_one_as_text_for_1()
         {
             string result = FizzBuzz.NumberToText(1);
             Assert.AreEqual("one", result);
+        }
+
+        [Test]
+        public void returns_two_as_text_for_2()
+        {
+            var result = FizzBuzz.NumberToText(2);
+            Assert.AreEqual("two", result);
         }
     }
 }
