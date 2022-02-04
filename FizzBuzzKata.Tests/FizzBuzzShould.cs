89226f253a174fbe1755b65cf98d58f22353d44a
jvaquero <jvaquero@aidacanarias.com>
2022-02-04 07:40:18 +0000

    Refactor tests - Use fixture data with a generic test

--- FizzBuzzKata.Tests/FizzBuzzShould.cs
+++ FizzBuzzKata.Tests/FizzBuzzShould.cs
@@ -1,90 +1,49 @@
-﻿using System.Diagnostics;
+﻿using System.Collections;
+using System.Diagnostics;
 using NUnit.Framework;
 
 namespace FizzBuzzKata.Tests {
+    [TestFixtureSource(typeof(MyFixtureData), nameof(MyFixtureData.Fixtureparams))]
     public class FizzBuzzShould {
-        [Test]
-        public void returns_1_as_string()
+        private readonly int value;
+        private readonly string expectedResult;
+
+        public FizzBuzzShould(int value, string expectedResult)
         {
-            string result = FizzBuzzer.FizzBuzz(1);
-            Assert.AreEqual("1", result);
+            this.value = value;
+            this.expectedResult = expectedResult;
         }
 
         [Test]
-        public void returns_2_as_string()
+        public void returns_expected_string_value()
         {
-            var result = FizzBuzzer.FizzBuzz(2);
-            Assert.AreEqual("2", result);
+            var result = FizzBuzzer.FizzBuzz(value);
+            Assert.AreEqual(expectedResult, result);
         }
+    }
 
-        [Test]
-        public void returns_FIZZ_for_3()
-        {
-            var result = FizzBuzzer.FizzBuzz(3);
-            Assert.AreEqual("FIZZ", result);
-        }
-
-        [Test]
-        public void returns_4_as_string()
-        {
-            var result = FizzBuzzer.FizzBuzz(4);
-            Assert.AreEqual("4", result);
-        }
-
-        [Test]
-        public void returns_BUZZ_for_5()
-        {
-            var result = FizzBuzzer.FizzBuzz(5);
-            Assert.AreEqual("BUZZ", result);
-        }
-
-        [Test]
-        public void returns_FIZZ_for_6()
-        {
-            var result = FizzBuzzer.FizzBuzz(6);
-            Assert.AreEqual("FIZZ", result);
-        }
-
-        [Test]
-        public void returns_FIZZ_for_9()
-        {
-            var result = FizzBuzzer.FizzBuzz(9);
-            Assert.AreEqual("FIZZ", result);
-        }
-
-        [Test]
-        public void returns_BUZZ_for_10()
-        {
-            var result = FizzBuzzer.FizzBuzz(10);
-            Assert.AreEqual("BUZZ", result);
-        }
-
-        [Test]
-        public void returns_FIZZ_for_20()
-        {
-            var result = FizzBuzzer.FizzBuzz(20);
-            Assert.AreEqual("BUZZ", result);
-        }
-
-        [Test]
-        public void returns_FIZZBUZZ_for_15()
-        {
-            var result = FizzBuzzer.FizzBuzz(15);
-            Assert.AreEqual("FIZZBUZZ", result);
-        }
-
-        [Test]
-        public void returns_FIZZBUZZ_for_30()
-        {
-            var result = FizzBuzzer.FizzBuzz(30);
-            Assert.AreEqual("FIZZBUZZ", result);
-        }
-
-        [Test]
-        public void returns_FIZZBUZZ_for_45()
-        {
-            var result = FizzBuzzer.FizzBuzz(45);
-            Assert.AreEqual("FIZZBUZZ", result);
+    public class MyFixtureData
+    {
+        public static IEnumerable Fixtureparams {
+            get {
+                yield return new TestFixtureData(1,   "1");
+                yield return new TestFixtureData(2,   "2");
+                yield return new TestFixtureData(3,   "FIZZ");
+                yield return new TestFixtureData(4,   "4");
+                yield return new TestFixtureData(5,   "BUZZ");
+                yield return new TestFixtureData(6,   "FIZZ");
+                yield return new TestFixtureData(7,   "7");
+                yield return new TestFixtureData(8,   "8");
+                yield return new TestFixtureData(9,   "FIZZ");
+                yield return new TestFixtureData(10,  "BUZZ");
+                yield return new TestFixtureData(15,  "FIZZBUZZ");
+                yield return new TestFixtureData(20,  "BUZZ");
+                yield return new TestFixtureData(30,  "FIZZBUZZ");
+                yield return new TestFixtureData(45,  "FIZZBUZZ");
+                yield return new TestFixtureData(77,  "77");
+                yield return new TestFixtureData(99,  "FIZZ");
+                yield return new TestFixtureData(100, "BUZZ");
+            }
         }
     }
 }
